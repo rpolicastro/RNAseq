e2bd65a1fbfc6e077c719c6edff2297cf95bd3b9
rpolicastro <rpolicas@indiana.edu>
2019-07-08 11:55:42 -0400

    getting the run script working

--- run.sh
+++ run.sh
@@ -10,18 +10,6 @@ set -e
 
 source settings.conf
 
-## check settings
-
-if [ ! -f "${REPDIR}/main.py" ]; then echo "WARNING: Repository directory is invalid." && exit 1; fi
-if [ ! -d "${SEQDIR}" ]; then echo "WARNING: Sequence directory does not exist." && exit 1; fi
-if [ ! -f "$GENOME_GTF" ]; then echo "WARNING: Genome GTF does not exist." && exit 1; fi
-if [ ! -f "$GENOME_FASTA" ]; then echo "WARNING: Genome FASTA does not exist." && exit 1; fi
-if [ ! -f "$SAMPLE_SHEET" ]; then echo "WARNING: Sample sheet does not exist." && exit 1; fi
-if [ ! -n "$PROJECT_ID" ]; then echo "WARNING: Project ID not set." && exit 1; fi
-if [ ! -n "$PROJECT_NAME" ]; then echo "WARNING: Project name not set." && exit 1; fi
-if [ ! -n "$ORGANISM" ]; then echo "WARNING: Organism name not set." && exit 1; fi
-if [ ! -n "$CORES" ]; then echo "WARNING: Cores not set." && exit 1; fi
-
 ## Create important directories
 
 if [ ! -d "$OUTDIR" ]; then mkdir -p $OUTDIR; fi
@@ -44,7 +32,13 @@ if [ ! -f "${OUTDIR}/container/RNAseq_latest.sif" ]
 then
 	echo "...downloading singularity container: RNAseq_latest.sif"
 	singularity pull shub://rpolicastro/RNAseq
-	[ -f "${OUTDIR}/container/RNAseq_latest.sif" ]; then echo "...singularity container downloaded"; else exit 1; fi
+	if [ -f "${OUTDIR}/container/RNAseq_latest.sif" ]
+	then 
+		echo "...singularity container downloaded"
+	else 
+		echo "Container not found"
+		exit 1
+	fi
 else
 	echo "...singularity container already exists in ${OUTDIR}/container"
 fi
