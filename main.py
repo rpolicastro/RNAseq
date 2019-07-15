6a79db7a9b86abea612b5b0908b599fc1caa66a1
rpolicastro <rpolicas@indiana.edu>
2019-07-15 10:58:17 -0400

    removing nargs=1 from main.py to avoid storing strings as lists

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ parser = argparse.ArgumentParser(description = 'Settings for RNA-seq analysis wo
 
 ## add arguments to parser
 
-parser.add_argument('--projectID', action='store', nargs=1, type=str, required=True, help='Short project ID indicator.')
-parser.add_argument('--projectName', action='store', nargs=1, type=str, required=True, help='Descriptive project name.')
-parser.add_argument('--organism', action='store', nargs=1, type=str, required=True, help='Organism name.')
-parser.add_argument('--cores', action='store', nargs=1, type=int, required=True, help='Number of available cores/threads.')
-parser.add_argument('--outDir', action='store', nargs=1, type=str, required=True, help='Directory to output results.')
-parser.add_argument('--seqDir', action='store', nargs=1, type=str, required=True, help='Directory containing fastq files.')
-parser.add_argument('--sampleSheet', action='store', nargs=1, type=str, required=True, help='Directory and name of sample sheet.')
-parser.add_argument('--genomeFasta', action='store', nargs=1, type=str, required=True, help='Directory and name of genomic fasta file.')
-parser.add_argument('--genomeGTF', action='store', nargs=1, type=str, required=True, help='Directory and name of genomic GTF file.')
+parser.add_argument('--projectID', action='store', type=str, required=True, help='Short project ID indicator.')
+parser.add_argument('--projectName', action='store', type=str, required=True, help='Descriptive project name.')
+parser.add_argument('--organism', action='store', type=str, required=True, help='Organism name.')
+parser.add_argument('--cores', action='store', type=int, required=True, help='Number of available cores/threads.')
+parser.add_argument('--outDir', action='store', type=str, required=True, help='Directory to output results.')
+parser.add_argument('--seqDir', action='store', type=str, required=True, help='Directory containing fastq files.')
+parser.add_argument('--sampleSheet', action='store', type=str, required=True, help='Directory and name of sample sheet.')
+parser.add_argument('--genomeFasta', action='store', type=str, required=True, help='Directory and name of genomic fasta file.')
+parser.add_argument('--genomeGTF', action='store', type=str, required=True, help='Directory and name of genomic GTF file.')
 
 ## fetch supplied arguments to parser
 
